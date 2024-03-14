29c9c8146651fb50e3bb628826412b6f045314ee
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-14 11:20:44 +0100

    Try fix #2

--- src/elements/Image.jsx
+++ src/elements/Image.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 
-function generateImageRendering(render) {
-  const validRenderValues = ["auto", "crisp-edges", "pixelated"];
-  return validRenderValues.includes(render) ? render : null;
-}
-
-function generateAltText(alt, title) {
-  return alt ? alt : title ? `Title: ${title}` : null;
-}
-
-function getSizeValue(size) {
-  return size ? (size.endsWith("%") ? size : `${size}px`) : null;
-}
-
 function Image({ source, title, width, height, render, alt }) {
-  const altText = generateAltText(alt, title);
-  const titleText = title || null;
+  const altText = alt ?? (title ? `Title: ${title}` : null);
+  const titleText = title ?? null;
+  const validRenderValues = ["auto", "crisp-edges", "pixelated"];
+
+  const getSizeValue = (size) => {
+    return size ? (size.endsWith("%") ? size : `${size}px`) : null;
+  };
 
   const styles = {
     width: getSizeValue(width),
     minWidth: getSizeValue(width),
     height: getSizeValue(height),
     minHeight: getSizeValue(height),
-    imageRendering: generateImageRendering(render),
+    imageRendering: validRenderValues.includes(render) ? render : null,
   };
 
   return <img alt={altText} title={titleText} src={source} style={styles} />;
