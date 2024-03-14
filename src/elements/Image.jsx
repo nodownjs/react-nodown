99daff9ba6600ed19f16797adbed1edf5ab73a20
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-03-14 12:36:45 +0100

    Image redo

--- src/elements/Image.jsx
+++ src/elements/Image.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 
 function Image({ source, title, width, height, render, alt }) {
-  const altText = alt ?? (title ? `Title: ${title}` : null);
-  const titleText = title ?? null;
-  const validRenderValues = ["auto", "crisp-edges", "pixelated"];
-
-  const getSizeValue = (size) => {
-    return size ? (size.endsWith("%") ? size : `${size}px`) : null;
-  };
+  const altText = generateAltText(alt, title);
+  const titleText = title || null;
 
   const styles = {
     width: getSizeValue(width),
     minWidth: getSizeValue(width),
     height: getSizeValue(height),
     minHeight: getSizeValue(height),
-    imageRendering: validRenderValues.includes(render) ? render : null,
+    imageRendering: generateImageRendering(render),
   };
 
   return <img alt={altText} title={titleText} src={source} style={styles} />;
+}
+
+function generateImageRendering(render) {
+  const validRenderValues = ["auto", "crisp-edges", "pixelated"];
+  return validRenderValues.includes(render) ? render : null;
+}
+
+function generateAltText(alt, title) {
+  return alt ? alt : title ? `Title: ${title}` : null;
+}
+
+function getSizeValue(size) {
+  return size ? (size.endsWith("%") ? size : `${size}px`) : null;
 }
 
 export default Image;
